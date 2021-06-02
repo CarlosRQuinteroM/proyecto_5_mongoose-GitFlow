987941ea1e28e358b13685356a94184f6fd89abc
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-06-02 16:36:03 +0200

    Add router / rest in the file routers.js and add. "start": "nodemon index.js", in package.json

--- router.js
+++ router.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRouter = require('./routes/userRouter');
+const restRouter = require('./routes/restaurantRouter')
 
 
 router.use('/user', userRouter);
+router.use('/rest', restRouter);
 
 
 module.exports = router;
