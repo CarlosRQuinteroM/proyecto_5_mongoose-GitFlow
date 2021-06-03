38dced4498c0d8dc1b7f9b7d4a1e1c2be20a051b
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-06-03 10:53:31 +0200

    creating more details about bookings

--- controllers/restController.js
+++ controllers/restController.js
@@ -15,19 +15,18 @@ class Local {
 
         const id = data.id;
         const userId = data.userId;
-
         const usuarioName = await User.findById(userId);
+        
 
         let booking = {
             idUser: data.userId,
             usuario: usuarioName.name,
-            text: data.texto,
+            comensales: data.comensales,
             fecha: data.fecha,
-            
-            
+            hora: data.hora,
+            alergias: data.alergias,
+            comentarios:data.comentarios,
         };
-
-        console.log(booking);
 
         return Rest.findByIdAndUpdate(
             {_id: id},
@@ -37,7 +36,8 @@ class Local {
 
     async deleteBooking(id){
         
-        return Room.findByIdAndRemove(id);
+        return Rest.findByIdAndRemove(id);
+        
     }
 
 }
