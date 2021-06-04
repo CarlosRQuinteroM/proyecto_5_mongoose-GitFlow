88c46e96ed2ab8c69c5ab698f9a714f0c910b956
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-06-04 17:52:13 +0200

    Delete extra data  create booking in arrays

--- controllers/restController.js
+++ controllers/restController.js
@@ -12,35 +12,9 @@ class Local {
         return Rest.create(rest);
     }
 
-    // async addBooking(data){
-
-    //     const id = data.id;
-    //     const userId = data.userId;
-    //     const usuarioName = await User.findById(userId);
-        
-
-    //     let booking = {
-    //         idUser: data.userId,
-    //         usuario: usuarioName.name,
-    //         comensales: data.comensales,
-    //         fecha: data.fecha,
-    //         hora: data.hora,
-    //         alergias: data.alergias,
-    //         comentarios:data.comentarios,
-    //     };
-
-    //     return Rest.findByIdAndUpdate(
-    //         {_id: id},
-    //         {$push: {bookings: booking}});
-        
-    // }
+    // borrar restaurantes // 
 
 
-    // async deleteBooking(id,idRestaurante){
-    //     return Rest.findByIdAndUpdate(
-    //         {_id : idRestaurante},
-    //         {$pull: {bookings: {idUser: id}}});
-    // }
 
 }
 
