9bac762fb351490e13f5599a9b34a0dfcec2017a
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-06-04 17:53:56 +0200

    clean code  Delete router booking in restaurant file

--- routes/restaurantRouter.js
+++ routes/restaurantRouter.js
@@ -5,7 +5,8 @@ const auth = require("../middlewares/auth");
 
 //GET - Return all Restaurants in the DB
 
-router.get("/",auth, async (req, res) => {
+
+router.get("/", async (req, res) => {
   try {
     res.json(await restController.findAllRests());
   } catch (err) {
@@ -16,7 +17,7 @@ router.get("/",auth, async (req, res) => {
 });
 
 //POST - Creates a new chat Restaurant
-
+// admin
 router.post("/create", async (req, res) => {
   try {
     const rest = req.body;
@@ -29,27 +30,19 @@ router.post("/create", async (req, res) => {
 });
 
 
-router.post("/add", async (req, res) => {
-  try {
-    const data = req.body.id;
-    res.json(await restController.addBooking(data));
-  } catch (err) {
-    return res.status(500).json({
-      message: err.message,
-    });
-  }
-});
 
-router.delete("/", async (req, res) => {
-  try {
-    const id = req.body.id;
-    const idRestaurante = req.body.idRestaurante;
-    res.json(await restController.deleteBooking(id,idRestaurante));
-  } catch (err) {
-    return res.status(500).json({
-      message: err.message,
-    });
-  }
-});
+// delete restaurante  (admin)
+
+// router.delete("/", async (req, res) => {
+//   try {
+//     const id = req.body.id;
+//     const idRestaurante = req.body.idRestaurante;
+//     res.json(await restController.deleteBooking(id,idRestaurante));
+//   } catch (err) {
+//     return res.status(500).json({
+//       message: err.message,
+//     });
+//   }
+// });
 
 module.exports = router;
