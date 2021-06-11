a737d5f64f1a758b54b657d674233890c664d526
juanfegallego <juanfegallegodj@gmail.com>
2021-06-11 11:28:15 +0200

    test

--- models/restaurant.js
+++ models/restaurant.js
@@ -7,10 +7,6 @@ const restSchema = new Schema({
     type: String,
     required: true,
   },
-  country: {
-    type: String,
-    required: true,
-  },
   city: {
     type: String,
     required: true,
