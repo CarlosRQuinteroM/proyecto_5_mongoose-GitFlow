fc7da6e7638c253be784b4b494b3411cc5050827
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-06-14 16:27:28 +0200

    change port Number

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('./router')
 const db = require('./config/mongoose.js');
 const app = express();
-const port = 3000;
+const port = 3005;
 const cors = require('cors');
 
 
